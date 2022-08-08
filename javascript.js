4c6ba8a7ae481fed71801a54b3a0ded19eca3054
salmgr87 <96929835+salmgr87@users.noreply.github.com>
2022-08-07 20:53:55 -0400

    Add button click JS and start playing functions.

---
+++ javascript.js
@@ -0,0 +1,39 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const container = document.querySelector('#container');
+
+
+const rockClicked = document.querySelector('#rock');
+rockClicked.addEventListener('click', function (e) {
+    playGame('rock');
+});
+
+const paperClicked = document.querySelector('#paper');
+paperClicked.addEventListener('click', function (e) {
+    playGame('paper');
+});
+
+const scissorsClicked = document.querySelector('#scissors');
+scissorsClicked.addEventListener('click', function (e) {
+    playGame('scissors');
+});
+
+
+function computerPlay() {
+    const moves = ["rock", "paper", "scissors"]
+    const computerMove = Math.floor(Math.random()*3);
+    let finalMove = moves[computerMove];
+    return finalMove;
+}
+
+
+function playGame(selection) {
+    let computerMove = computerPlay();
+    console.log(`The computer played  ${computerMove}`);
+    if (selection == computerPlay()) {
+        console.log("It's a tie. Play again.");
+    } else if ((selection == 'rock' && computerMove == 'scissors') || (selection == 'paper' && computerMove == 'rock') || (selection == 'scissors' && computerMove == 'paper')) {
+        console.log("You won this round");
+    }
+}
